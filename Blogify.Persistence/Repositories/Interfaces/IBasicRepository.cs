815f9709349c4a4eaf48c604dc7a7a1eb3b209cc
oreze <kunas1@o2.pl>
2022-02-01 18:08:51 +0100

    Implement PostsRepository, skip update methods

--- Blogify.Persistence/Repositories/Interfaces/IBasicRepository.cs
+++ Blogify.Persistence/Repositories/Interfaces/IBasicRepository.cs
@@ -3,9 +3,14 @@
 public interface IBasicRepository<T> where T: class
 {
     public IEnumerable<T> GetAll(int page, int offset);
-    public T GetById(long id);
+    public T GetById(Guid id);
     public void Insert(T entity);
     public void Update(T entity);
-    public void Delete(long id);
-    public void Save();
+    public void Delete(Guid id);
+    
+    public Task<IEnumerable<T>> GetAllAsync(int page, int offset);
+    public Task<T> GetByIdAsync(Guid id);
+    public Task InsertAsync(T entity);
+    public Task UpdateAsync(T entity);
+    public Task DeleteAsync(Guid id);
 }
