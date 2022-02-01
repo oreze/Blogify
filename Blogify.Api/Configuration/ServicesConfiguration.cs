614f5b3a0b15130911345379b2fce6a4a0846038
oreze <kunas1@o2.pl>
2022-02-01 19:17:29 +0100

    Configure services

---
+++ Blogify.Api/Configuration/ServicesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Blogify.Domain.Data;
+using Blogify.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogify.Configuration;
+
+public class ServicesConfiguration
+{
+    public static void ConfigureServices(ref WebApplicationBuilder app)
+    {
+        var connectionString = app.Configuration.GetConnectionString("BlogDb");
+        app.Services.AddDbContext<BlogDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        app.Services.AddScoped<IPostsRepository, PostsRepository>();
+        app.Services.AddScoped<IUsersRepository, IUsersRepository>();
+    }
+}
